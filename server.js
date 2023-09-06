af9c3fd568a36b76ebacf395a522d0ca07f55ae0
ranggarahman <rangga.a.r@mail.ugm.ac.id>
2023-09-06 22:14:20 +0700

    errorHandler and Logger file added

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const {logger} = require('./middleware/logger')
+const { log } = require('console')
+const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500
+
+app.use(logger)
 
 app.use(express.json())
 
@@ -20,4 +25,20 @@ app.all('*', (req, res) => {
     }
 })
 
-app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))
+app.use(errorHandler)
+
+const server = app.listen(PORT, () => {
+    console.log(`Server Running on Port ${PORT}`);
+});
+
+// Graceful shutdown on SIGINT (Ctrl+C) signal
+process.on('SIGINT', () => {
+    console.log('Server shutting down gracefully...');
+    
+    // Perform any necessary cleanup tasks here, like closing database connections, releasing resources, etc.
+
+    server.close(() => {
+        console.log('Server has been gracefully terminated.');
+        process.exit(0); // Exit the process gracefully
+    });
+});
