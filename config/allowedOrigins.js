b9df6bc22c16b9a9013f57f30235a82d63d24c44
ranggarahman <rangga.a.r@mail.ugm.ac.id>
2023-09-07 15:43:52 +0700

    mongoDB initialization

---
+++ config/allowedOrigins.js
@@ -0,0 +1,3 @@
+const allowedOrigins = [
+    'https://localhost:3000'
+]
--- server.js
+++ server.js
@@ -1,14 +1,26 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const {logger} = require('./middleware/logger')
+const {logger, logEvents} = require('./middleware/logger')
 const { log } = require('console')
 const errorHandler = require('./middleware/errorHandler')
+const cors = require('cors')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3500
+const corsOptions = require('./config/corsOptions')
+const cookieParser = require('cookie-parser')
+
+console.log(process.env.NODE_ENV)
+
+connectDB()
 
 app.use(logger)
 
+app.use(cors(corsOptions))
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, '/public')))
 app.use('/', require('./routes/root'))
@@ -27,18 +39,29 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler)
 
-const server = app.listen(PORT, () => {
-    console.log(`Server Running on Port ${PORT}`);
-});
 
-// Graceful shutdown on SIGINT (Ctrl+C) signal
-process.on('SIGINT', () => {
-    console.log('Server shutting down gracefully...');
+mongoose.connection.once('open', () => {
+    console.log('connected to MongoDB')
+    app.listen(PORT, () => {
+        console.log(`Server Running on Port ${PORT}`);
+    })
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    'mongoErrLog.log')
+})
+
+
+// // Graceful shutdown on SIGINT (Ctrl+C) signal
+// process.on('SIGINT', () => {
+//     console.log('Server shutting down gracefully...');
     
-    // Perform any necessary cleanup tasks here, like closing database connections, releasing resources, etc.
+//     // Perform any necessary cleanup tasks here, like closing database connections, releasing resources, etc.
 
-    server.close(() => {
-        console.log('Server has been gracefully terminated.');
-        process.exit(0); // Exit the process gracefully
-    });
-});
+//     server.close(() => {
+//         console.log('Server has been gracefully terminated.');
+//         process.exit(0); // Exit the process gracefully
+//     });
+// });
